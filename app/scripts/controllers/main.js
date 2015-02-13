871b692121f12ea7519e95ff316612724cc1e8d4
flareair <flareair0@gmail.com>
2015-02-14 00:24:17 +0300

    Big improvements in app functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,17 @@
  * Controller of the creditCalcAngularApp
  */
 angular.module('creditCalcAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.banks = [];
-  });
+  .controller('MainCtrl', MainCtrl);
+
+
+function MainCtrl($scope, banks) {
+  $scope.current = {};
+  $scope.banks = banks.getBanks();
+  $scope.initial = $scope.banks[0];
+  $scope.basePrice = 1250000;
+
+  $scope.calculate = function() {
+    console.log($scope.current.bank.name);
+  };
+  
+}
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,8 +5,7 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('creditCalcAngularApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
@@ -16,7 +15,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    // expect(scope.awesomeThings.length).toBe(3);
+  it('Should get bank list from service', function () {
+    expect(scope.banks.length).toBe(2);
   });
 });
